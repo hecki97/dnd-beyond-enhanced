06d53fef0df0d835ce63a6da6a17e74ed6a208b4
Y.Heckmann <thehecki97@gmail.com>
2021-06-25 18:22:29 +0200

    feat: extracted util functions in own file

--- src/script.js
+++ src/script.js
@@ -1,33 +1,7 @@
 /* global marked */
+const { elementReady, createHyperlink } = window.DDBE_Util;
 
 let noteObserver = null;
-
-/**
- * Waits for an element satisfying selector to exist, then resolves promise with the element.
- *
- * Based on this gist by [jwilson8767](https://gist.github.com/jwilson8767): https://gist.github.com/jwilson8767/db379026efcbd932f64382db4b02853e
- *
- * @param {string} selector valid html selector
- * @returns {Promise<Element>} HTML element
- */
-function elementReady(selector) {
-  return new Promise((resolve) => {
-    Array.from(document.querySelectorAll(selector)).forEach((element) => {
-      resolve(element);
-    });
-
-    const mutObserver = new MutationObserver((mutationRecords, observer) => {
-      // Query for elements matching the specified selector
-      Array.from(document.querySelectorAll(selector)).forEach((element) => {
-        resolve(element);
-        // Once we have resolved we don't need the observer anymore.
-        observer.disconnect();
-      });
-    });
-
-    mutObserver.observe(document.documentElement, { childList: true, subtree: true });
-  });
-}
 
 /**
  * Creates and injects a new tab option that opens an external resource for Markdown into the page
@@ -35,13 +9,11 @@ function elementReady(selector) {
  * @returns void
  */
 function injectExternalMarkdownResource() {
-  const anchor = document.createElement('a');
-  anchor.href = 'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet';
-  anchor.text = 'MD Cheatsheet';
-  anchor.rel = 'nofollow';
-  anchor.type = 'noopener';
-  anchor.target = '_blank';
-  anchor.classList.add('ddbc-tab-options__header-heading');
+  const anchor = createHyperlink(
+    'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet',
+    'MD Cheatsheet',
+    'ddbc-tab-options__header-heading',
+  );
 
   const option = document.createElement('div');
   option.classList.add('ddbc-tab-options__header');
---
+++ src/util.js
@@ -0,0 +1,36 @@
+window.DDBE_Util = {
+  /**
+   * Waits for an element satisfying selector to exist, then resolves promise with the element.
+   *
+   * Based on this gist by [jwilson8767](https://gist.github.com/jwilson8767): https://gist.github.com/jwilson8767/db379026efcbd932f64382db4b02853e
+   *
+   * @param {string} selector valid html selector
+   * @returns {Promise<Element>} HTML element
+   */
+  elementReady: (selector) => new Promise((resolve) => {
+    Array.from(document.querySelectorAll(selector)).forEach((element) => {
+      resolve(element);
+    });
+
+    const mutObserver = new MutationObserver((mutationRecords, observer) => {
+      // Query for elements matching the specified selector
+      Array.from(document.querySelectorAll(selector)).forEach((element) => {
+        resolve(element);
+        // Once we have resolved we don't need the observer anymore.
+        observer.disconnect();
+      });
+    });
+
+    mutObserver.observe(document.documentElement, { childList: true, subtree: true });
+  }),
+
+  createHyperlink: (href, text, ...classList) => {
+    const anchor = document.createElement('a');
+    anchor.href = href;
+    anchor.text = text;
+    anchor.rel = 'nofollow';
+    anchor.type = 'noopener';
+    anchor.target = '_blank';
+    anchor.classList.add(classList);
+  },
+};
