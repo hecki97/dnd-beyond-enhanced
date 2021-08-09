3024f8d9e8bf1d01ee1877b5c83dc87da0ceb8ab
Y.Heckmann <thehecki97@gmail.com>
2021-08-09 17:16:03 +0200

    feat: added injectMarkdownHyperlink()

--- src/markdown-renderer.ts
+++ src/markdown-renderer.ts
@@ -1,5 +1,24 @@
 namespace DnDBeyondEnhanced {
   export class MarkdownRenderer {
-    constructor() {}
+    /**
+     * Creates and injects a new tab option that opens an external resource for Markdown into the page
+     *
+     * @returns void
+     */
+    public injectMarkdownHyperlink() {
+      const anchor = createHyperlink(
+        'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet',
+        'MD Cheatsheet',
+        'ddbc-tab-options__header-heading',
+      );
+
+      const tabListItem = document.createElement('div');
+      tabListItem.classList.add('ddbc-tab-options__header');
+      tabListItem.style = 'margin-left: auto;';
+      tabListItem.appendChild(anchor);
+
+      const notesPrimaryTabList = document.querySelector('.ct-notes .ddbc-tab-options__nav');
+      notesPrimaryTabList.appendChild(tabListItem);
+    }
   }
 }
