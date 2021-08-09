162bdca468a0d248b3baba0e0d7d089c8e6c3546
Y.Heckmann <thehecki97@gmail.com>
2021-08-09 17:19:19 +0200

    feat: added observePrimaryBox()

--- src/markdown-injector.ts
+++ src/markdown-injector.ts
@@ -1,13 +1,30 @@
 namespace DnDBeyondEnhanced {
-    export class MarkdownInjector {
-        constructor() {
-    
+  export class MarkdownInjector {
+    private primaryBoxObserver: MutationObserver;
+    private activeObserver: MutationObserver;
+
+    public observePrimaryBox() {
+      this.primaryBoxObserver = new MutationObserver(([first, second]) => {
+        const activeListItem = this.hasIsActiveClass(first.target) ? first.target : second.target;
+
+        if (this.activeObserver) {
+          this.activeObserver.disconnect();
         }
-    
-        observePrimaryBox() {
-    
+
+        switch (activeListItem.textContent) {
+          case 'Notes':
+            this.injectMarkdownHyperlink();
+            this.registerNoteObserver(
+              document.querySelector('.ct-notes .ct-content-group:last-child div.ct-notes__note'),
+              convertContentToMarkdown,
+            );
+            break;
+          case 'Inventory':
+            break;
+          default:
+            break;
         }
-    
+      });
         disconnectPrimaryBoxObserver() {
     
         }
